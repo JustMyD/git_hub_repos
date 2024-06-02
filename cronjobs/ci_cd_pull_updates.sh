596f91b2e83a516df8fa52dc95a6c4258c1d84b9
JustMyD <commonqued@gmail.com>
2024-06-02 17:06:01 +0300

    обновил задачи

--- cronjobs/ci_cd_pull_updates.sh
+++ cronjobs/ci_cd_pull_updates.sh
@@ -16,11 +16,9 @@ where part.rn = 1 and part.IsProcessed = 0 and config.Status = 1
 for (( i=2; i<"${#queryResults[@]} - 1"; i++ )) do
     internalId="$(echo "${queryResults[i]}" | cut -d "|" -f1 | tr -d ' ')"
     projectName="$(echo "${queryResults[i]}" | cut -d "|" -f2 | tr -d ' ')"
-    portForward="$(echo "${queryResults[i]}" | cut -d "|" -f3)"
     projectPath="$(echo "${queryResults[i]}" | cut -d "|" -f4 | tr -d ' ')"
-    echo "$internalId" "$projectName" "$portForward"
     
-    cd $projectPath/$projectName &&
+    cd $projectPath &&
     git checkout master &&
     git pull &&
     psql -d main -U comediant -c "update git.queue set IsProcessed = 1 where RepositoryName = '$projectName' and InternalId = '$internalId'"
